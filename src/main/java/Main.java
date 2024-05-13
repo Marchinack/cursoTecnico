10a485e2f1db99366331f05363219ad404288318
Mateus Marchinack <167545286+Marchinack@users.noreply.github.com>
2024-05-13 16:59:55 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("       Lógica computacional");
+    System.out.println("");
+    System.out.println("   Lógica computacional é uma matéria bem divertida, difícil e facil ao mesmo tempo.");
+    System.out.println("Eu tenho um pouco de dificuldade na parte da tabéla verdade mas não é tão difícil.");
+    System.out.println("   Recentemente nós começamos a escrever códigos.");
+      System.out.println("Até agora nós só estamos usando o comando print da linguagem java");
+    System.out.println("   Não é muito difícil mas não é facil também, pois se você esquecer uma uníca palavra, o codigo já não funciona.");
+      System.out.println("     Conclusão:");
+    System.out.println("");
+    System.out.println("   Lógica computacional é uma matéria bem divertida mas se você não prestar atenção vai se perder e não vai saber o que fazer.");
+  
+  
+  }
+}
